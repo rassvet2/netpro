0fbc51a1b7d474c091ca15587bb12cd785e6cea3
rassvet_ii <dekabrist19480705@gmail.com>
2025-04-11 11:32:04 +0900

    Add Hello.java

--- app/src/main/java/tdu_iwai/App.java
+++ app/src/main/java/tdu_iwai/App.java
@@ -4,11 +4,8 @@
 package tdu_iwai;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Hello Network Programing and Cloud System Deveopment World.");
+        Hello.main(args);
     }
 }
---
+++ app/src/main/java/tdu_iwai/Hello.java
@@ -0,0 +1,7 @@
+package tdu_iwai;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.err.println("Hello World!");
+    }
+}
