3eba520db0c547cbed73f3ba6e7e7c36a0b26389
rassvet_ii <dekabrist19480705@gmail.com>
2025-04-18 10:37:43 +0900

    Add 0418 task B

---
+++ app/src/main/java/tdu_iwai/BugFix1.java
@@ -0,0 +1,9 @@
+public class BugFix1 {
+	public static void main(String[] args) {
+		System.out.println("Hello! \"木村君\" \n ");
+		System.out.print("A");
+		System.out.print("B");
+		System.out.print("C");
+	}//main end
+
+// ヒント　中括弧を開いたら閉じる。対応する（）や｛｝にはコメントを入れよう。//main end
---
+++ app/src/main/java/tdu_iwai/BugFix4.java
@@ -0,0 +1,7 @@
+public class BugFix4 {
+	public static void main(String[] args) {
+		println("Hello! \"木村君\" \n ");
+		print("A");
+		print(B)
+		print("C');
+}
